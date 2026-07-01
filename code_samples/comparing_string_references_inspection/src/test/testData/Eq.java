1de897734d1334b09e874541dfe312daad3801b8
moyann29 <moyann29@gmail.com>
2026-07-02 02:15:03 +0800

    Delete code_samples directory

--- code_samples/comparing_string_references_inspection/src/test/testData/Eq.java
+++
@@ -1,5 +0,0 @@
-public class Eq {
-    public boolean compareStrings(String s1, String s2) {
-        return (<caret>s1 == s2);
-    }
-}
--- code_samples/comparing_string_references_inspection/src/test/testData/Neq.java
+++
@@ -1,5 +0,0 @@
-public class Neq {
-  public boolean compareStrings(String s1, String s2) {
-    return (s1 <caret>!= s2);
-  }
-}
